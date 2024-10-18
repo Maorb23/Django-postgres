5f6a2532e25ca7945cb05a7266589e2855182b8b
Maorb23 <146326556+Maorb23@users.noreply.github.com>
2024-10-18 15:24:58 +0300

    Update wsgi.py

--- Maor_proj/mysite/wsgi.py
+++ Maor_proj/mysite/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 from django.conf import settings
     
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Maor_proj.mysite.settings')
 
 application = get_wsgi_application()
 #application = WhiteNoise(application, root='/path/to/staticfiles')
